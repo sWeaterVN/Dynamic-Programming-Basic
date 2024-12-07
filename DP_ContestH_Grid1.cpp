1131f1d2da84b1c703c6f29c774ef27735fd4ce1
sWeaterVN <sweater208@gmail.com>
2024-12-07 09:56:51 +0700

    Create DP_ContestH_Grid1.cpp

---
+++ DP_ContestH_Grid1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int h, w;
+char a[1001][1001];
+int dp[1001][1001];
+const long long N=1e9+7;
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin>>h>>w;
+    for(int i=1; i<=h; i++){
+        for(int j=1; j<=w; j++){
+            cin>>a[i][j];
+        }
+    }
+    dp[1][1]=1;
+    for(int i=1; i<=h; i++){
+        for(int j=1; j<=w; j++){
+            if(i==1 && j==1) continue;
+            if(a[i][j]=='.')
+                dp[i][j]=(dp[i-1][j]+dp[i][j-1])%N;
+            if(a[i][j]=='#')
+                dp[i][j]=0;
+        }
+    }
+    cout<<dp[h][w];
+    return 0;
+}
